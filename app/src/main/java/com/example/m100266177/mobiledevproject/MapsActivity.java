c3548affe6a4b27848fc7505da34bd99f855b848
James Gelinas <james.gelinas@uoit.net>
2015-11-30 22:32:58 -0500

    Final push, included more commenting, plans for the future with the app, Changed the location the map zooms in towards to Toronto as it is more of a central hub.

--- app/src/main/java/com/example/m100266177/mobiledevproject/MapsActivity.java
+++ app/src/main/java/com/example/m100266177/mobiledevproject/MapsActivity.java
@@ -5,6 +5,14 @@ package com.example.m100266177.mobiledevproject;
  * of this map. In order for me to run this properly however I had to download GenyMotion and use
  * it as an emulator as I can install google play services on that. A sample map marker for UOIT
  * has been put onto the map and the camera zooms in towards it.
+ *
+ * I ran out of time to do the geocoding properly because I had a ton of problems getting the map
+ * activity to run for testing. Its mainly because I started late and I take responsibility for that.
+ * The plan was when the user pressed the "Get Directions" button to pass the address over through
+ * an intent, chop it up into single words and make run it through to the google geocoding API. This
+ * would be done in a similar fashion to the Forecast class except it would all be behind the scenes
+ * and the only result would be a red pin in the map where the persons event was. The user could then
+ * click the directions button and continue from there.
  */
 
 import android.support.v4.app.FragmentActivity;
@@ -45,7 +53,7 @@ public class MapsActivity extends FragmentActivity {
     }
 
     private void setUpMap(){
-        mMap.addMarker(new MarkerOptions().position(new LatLng(43.9448, -78.8917)).title("UOIT"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(43.9448, -78.8917), 14.9f));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(43.7, -79.4)).title("TORONTO"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(43.7, -79.4), 14.9f));
     }
 }
